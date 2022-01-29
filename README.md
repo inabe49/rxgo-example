03abf475dd939981f78228b5ea3b61ef969cddce
Toshiaki Inahata <darwin49@gmail.com>
2022-01-29 16:38:39 +0900

    試作

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # rxgo-example
+
 RxGo の使い方のサンプル
+
+## プロジェクトの初期化
+
+```
+go mod init github.com/inabe49/rxgo-example
+```
+
+## RxGo のインストール
+
+```
+go get -u github.com/reactivex/rxgo/v2
+```
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/reactivex/rxgo/v2"
+	"time"
+)
+
+func AsCancelable(observable rxgo.Observable) (func(), rxgo.Observable) {
+	canceled := false
+	cancel := func() {
+		canceled = true
+	}
+
+	return cancel, observable.Map(func(ctx context.Context, i interface{}) (interface{}, error) {
+		if canceled {
+			return nil, fmt.Errorf("canceled")
+		}
+
+		return i, nil
+	})
+}
+
+func main() {
+	ch := make(chan rxgo.Item)
+	observable := rxgo.FromEventSource(ch, rxgo.WithBackPressureStrategy(rxgo.Drop))
+
+	// ctx, cancel := context.WithCancel(context.Background())
+
+	ch <- rxgo.Of(1)
+	ch <- rxgo.Of(2)
+	ch <- rxgo.Of(3)
+
+	go func() {
+		for i := 4; i < 10; i++ {
+			ch <- rxgo.Of(i)
+			<-time.After(1 * time.Second)
+		}
+
+		close(ch)
+	}()
+
+	go func() {
+		// Observer 1
+		observable.ForEach(func(i interface{}) {
+			println("Observer 1 : value : ", i.(int))
+		}, func(err error) {
+			println("Observer 1 : error : ", err.Error())
+		}, func() {
+			println("Observer 1 : finish")
+		})
+	}()
+
+	go func() {
+		// Observer 2
+		observable.ForEach(func(i interface{}) {
+			println("Observer 2 : value : ", i.(int))
+		}, func(err error) {
+			println("Observer 2 : error : ", err.Error())
+		}, func() {
+			println("Observer 2 : finish")
+		})
+	}()
+
+	for item := range observable.Map(func(ctx context.Context, i interface{}) (interface{}, error) {
+		return nil, fmt.Errorf("error")
+	}).Observe() {
+		println(item.E)
+	}
+	observable.Observe()
+
+	/*
+		for item := range observable.Observe() {
+			println(item.V.(int))
+
+			<-time.After(2 * time.Second)
+		}
+	*/
+
+	<-time.After(10 * time.Second)
+}
